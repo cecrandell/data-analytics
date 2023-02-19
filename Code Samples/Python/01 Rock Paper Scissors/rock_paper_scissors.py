1c0dd6661db27a88d793aabd017dba1cb6e10d31
Caroline Crandell <cecrandell19@gmail.com>
2023-02-19 08:58:27 -0500

    01 Rock Paper Scissors

---
+++ Code Samples/Python/01 Rock Paper Scissors/rock_paper_scissors.py
@@ -0,0 +1,48 @@
+import random
+
+print("Let's play!")
+
+options = ["r", "p", "s"]
+
+computer_choice = random.choice(options)
+
+user_choice = input("Please type: r for rock, p for paper, or s for scissors")
+
+if (user_choice == "r" and computer_choice == "p"):
+    print("You: rock. Computer: paper.")
+    print("You lose!")
+
+elif (user_choice == "r" and computer_choice == "s"):
+    print("You: rock. Computer: scissors.")
+    print("You won!")
+
+elif (user_choice == "r" and computer_choice == "r"):
+    print("You: rock. Computer: rock.")
+    print("Tie!")
+
+elif (user_choice == "p" and computer_choice == "p"):
+    print("You: paper. Computer: paper.")
+    print("Tie!")
+
+elif (user_choice == "p" and computer_choice == "s"):
+    print("You: paper. Computer: scissors.")
+    print("You lose!")
+
+elif (user_choice == "p" and computer_choice == "r"):
+    print("You: paper. Computer: rock.")
+    print("You won!")
+
+elif (user_choice == "s" and computer_choice == "p"):
+    print("You: scissors. Computer: paper.")
+    print("You won!")
+
+elif (user_choice == "s" and computer_choice == "s"):
+    print("You: scissors. Computer: scissors.")
+    print("Tie!")
+
+elif (user_choice == "s" and computer_choice == "r"):
+    print("You: scissors. Computer: rock.")
+    print("You lose!")
+
+else:
+    print("Hmmmm, please try again: 'r', 'p', or 's'.")
--- Code Samples/Python/README.md
+++ Code Samples/Python/README.md
@@ -1,1 +1,9 @@
 # Python
+
+**Activities Included**
+
+- [01 Rock Paper Scissors](/Code%20Samples/Python/01%20Rock%20Paper%20Scissors)
+
+**Developer**
+
+- Caroline Crandell - cecrandell - cecrandell19@gmail.com - [LinkedIn](https://www.linkedin.com/in/carolinecrandell/)
